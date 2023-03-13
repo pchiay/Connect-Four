d0485787b04332d53d7299887282214eeb1ebcbb
pchiay <pacyan@ucdavis.edu>
2023-03-13 01:37:38 -0700

    Created a new file

---
+++ input_validation.h
@@ -0,0 +1,6 @@
+#ifndef INPUT_VALIDATION_H
+	#define INPUT_VALIDATION_H
+	#include <stdbool.h>
+	bool isValidFormat(const int numArgsRead, const int numArgsNeed);
+	int getValidInt(const char* prompt);
+#endif
